d1d1729852c510c7702820f69bccdf0bfcfb6c6e
John Tocher <jmtochergmail.com>
2025-12-23 11:48:31 +1100

    Puzzle 01 part 01 complete

--- Puzzle_01/puzzle_01-part_1_jmt.py
+++ Puzzle_01/puzzle_01-part_1_jmt.py
@@ -6,38 +6,51 @@ John Tocher
 Solution to puzzle 01 part 1
 """
 
-INPUT_FILE_NAME = "puzzle_01_input_01_sample.txt"
+INPUT_FILE_NAME = "puzzle_01_input_01.txt"
 
 
 def read_input_data():
     # Read the puzzle input from a text file
     count_lines = 0
-    lines_of_numbers = list()
-    list_a = list()
-    list_b = list()
+    list_of_rotations = list()
+
     for single_line in open(INPUT_FILE_NAME, "r").readlines():
         count_lines += 1
         clean_line = single_line.strip()
-        list_of_numbers = [int(each_num) for each_num in clean_line.split("   ")]
-        list_a.append(list_of_numbers[0])
-        list_b.append(list_of_numbers[1])
+        list_of_rotations.append(clean_line)
 
-    list_a.sort()
-    list_b.sort()
-
-    return list_a, list_b
+    return list_of_rotations
 
 
 def solve_puzzle():
     # Main solving logic
     puzzle_input = read_input_data()
+    # print(puzzle_input)
     assert len(puzzle_input[0]) == len(puzzle_input[1]), "Bad input!"
-    diff_total = 0
-    for line_index in range(0, len(puzzle_input[0])):
-        this_diff = abs(puzzle_input[0][line_index] - puzzle_input[1][line_index])
-        diff_total += this_diff
 
-    print(f"Result is: {diff_total}")
+    dial_pos = 50
+    count_moves = 0
+    count_at_zero = 0
+    for each_move in puzzle_input:
+        count_moves += 1
+        move_dir = each_move[0:1]
+        move_size = int(each_move[1:]) % 100
+        # print(f"Move {count_moves:04} is {each_move}")
+        if move_dir == "R":
+            move_distance = move_size
+        elif move_dir == "L":
+            move_distance = 100 - move_size
+        else:
+            assert False, f"Unexpected input at line: {count_moves}"
+        new_pos = (dial_pos + move_distance) % 100
+        if new_pos == 0:
+            count_at_zero += 1
+        print(
+            f"The dial is rotated {each_move} ({move_distance}) to point at {new_pos}."
+        )
+        dial_pos = new_pos
+
+    print(f"Result is: {count_at_zero} at zero")
 
 
 if __name__ == "__main__":
